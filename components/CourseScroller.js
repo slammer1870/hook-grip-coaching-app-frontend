804c4445cfeff97a057d6cecce7149e0f1d57187
Sam Mc Nally <sam.mcnally94@gmail.com>
2021-03-04 17:55:35 +0000

    height

--- components/CourseScroller.js
+++ components/CourseScroller.js
@@ -5,11 +5,12 @@ const CourseScroller = ({ course }) => {
   if (course) {
     return (
       <a href={`/courses/${course.slug}`}>
-        <div className="flex flex-col border w-52 h-82 my-4">
+        <div className="flex flex-col border w-52 h-auto
+         my-4">
           <div className="flex h-40 w-full items-center justify-center overflow-hidden">
             <img alt={course.title} src={fromImageToUrl(course.thumbnail)} />
           </div>
-          <div className="p-4 flex flex-col h-24">
+          <div className="p-4 flex flex-col">
             <h1 className="text-xl mb-1">{course.title}</h1>
             <p className="text-sm">{course.description}</p>
             <span className="ml-auto">
